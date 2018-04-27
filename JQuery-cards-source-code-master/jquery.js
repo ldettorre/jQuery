be2fef685cb0a9837dd97374c709f2641be611b1
Luca DEttorre <ldettorre21@gmail.com>
2018-04-27 16:04:49 +0000

    lights test

--- JQuery-cards-source-code-master/jquery.js
+++ JQuery-cards-source-code-master/jquery.js
@@ -1,15 +1,30 @@
-$(document).ready(function(){
-    
-    $("#myParagraph").click(function(){
-        for (let i=1; i<=10;i++){
-            $("#myParagraph").append("<p>This is just a test.</p>");
+$(document).ready(function() {
+    $(".switchOff").click(function() {
+        if ($(".bulbOff").attr("src") == "img/bulboff.gif") {
+            $(".bulbOff").attr("src", "img/bulbon.gif")
+            $(".switchOff").attr("src", "img/switchOn.png");
+            // $(".switchOff").addClass("switchOn").removeClass("switchOff");
+        }
+        else {
+            $(".bulbOff").attr("src", "img/bulboff.gif")
+            $(".switchOff").attr("src", "img/switchOff.png")
+            // $(".switchOn").addClass("switchOff").removeClass("switchOn")
+
         }
     })
-    
-    
-    $(".test").click(function(){
-        $("p").addClass("testing");
-
 })
 
-})
+
+
+
+// $("#myParagraph").click(function(){
+    //     for (let i=1; i<=10;i++){
+    //         $("#myParagraph").append("<p>This is just a test.</p>");
+    //     }
+    // })
+    
+    
+    // $(".test").click(function(){
+    //     $("p").addClass("testing");
+
+// })
